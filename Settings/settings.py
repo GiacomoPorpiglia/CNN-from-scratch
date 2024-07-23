e9bc369adce292291b5c9226aa72be5ba3f64d51
Giacomo Porpiglia <giacomo.porpiglia@gmail.com>
2024-07-23 14:27:33 +0200

    changed from sigmoid to relu(invalid net)

--- Settings/settings.py
+++ Settings/settings.py
@@ -11,7 +11,7 @@ LDNSize = [256, 120, 84, 10] #Size of the layer-dense part of the network (the f
 CNNSize = [[6, 5], [16, 5]] #each array contains [number of kernels, kernel_size]. The first layer is made of 6 kernels, each of 5x5 #NOTE: If you want to change the dimensiions of the layers, you will also need to change the combinations in combinationsMaps.py file
 optimizer = Adam #You can change it to Momentum, but I have experienced that Adam woks much better and has a quicker convergence.
 
-activation_function_conv_layer   = Activations.Sigmoid() #options: SIGMOID, RELU
+activation_function_conv_layer   = Activations.Relu() #options: SIGMOID, RELU
 activation_function_pool_layer   = Activations.Mean() #options: MEAN, MAX
-activation_function_layer_dense  = Activations.Sigmoid() # options: SIGMIOD / RELU
+activation_function_layer_dense  = Activations.Relu() # options: SIGMIOD / RELU
 acitvation_function_output_layer = Activations.Softmax() # options: SOFTMAX
